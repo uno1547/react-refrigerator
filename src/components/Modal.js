8ecc38ccd73d7fa998d5f21b738e9cacb2bcfaf1
Yuno Kim <unokim1547@gmail.com>
2024-12-02 01:28:42 +0900

    test - Modal component

---
+++ src/components/Modal.js
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+
+import styles from './Modal.module.css'
+import Button from './Button'
+
+const Modal = ({mainText, subText, close}) => {
+
+  return (
+  <div className={styles.background} onClick={close}>
+    <div className={styles.modal_container}>
+      <span className={styles.close} onClick={close}>&times;</span>
+      <h4>{mainText}</h4>
+      <span>{subText}</span>
+      <div className={styles.btn_area}>
+        {/* <button className={styles.cancle}>cancle</button> */}
+        {/* <button className={styles.run}>discard</button> */}
+        <Button text = 'cancle' handler={close}/>
+        <Button text = 'discard' handler={close}/>
+      </div>
+    </div>
+  </div>)
+  
+  
+  /*
+  // const [isOpen, setIsOpen] = useState(true)
+
+  // const onClickHandler = () => {
+  //   setIsOpen(false)
+  // }
+  return isOpen ? (
+    <div className={styles.background}>
+      <div className={styles.modal_container}>
+        <span className={styles.close} onClick={onClickHandler}>&times;</span>
+        <h4>{mainText}</h4>
+        <span>{subText}</span>
+        <div className={styles.btn_area}>
+          <button className={styles.cancle}>cancle</button>
+          <button className={styles.run}>discard</button>
+          <Button text = 'cancle' handler={onClickHandler}/>
+          <Button text = 'discard' handler={onClickHandler}/>
+        </div>
+      </div>
+    </div>) : null
+*/
+}
+export default Modal
--- src/components/Test1.js
+++ src/components/Test1.js
@@ -1,49 +1,23 @@
 import { useEffect, useState} from 'react'
-import getData from '../utils/getData'
+import { createPortal } from 'react-dom'
+// import getData from '../utils/getData'
+import Modal from './Modal'
 
 const Test1 = () => {
-  const [category, setCategory] = useState('date')
-  const [list, setList] = useState([])
-  const [loading, setLoading] = useState(false)
-
-  const fetchDatas = async () => {
-    const response = await getData()
-    setList(response)
-    setLoading(true)
+  const [open, setOpen] = useState(false)
+  const handler = (evt) => {
+    evt.stopPropagation()
+    setOpen(prev => !prev)
+    // console.log('닫힘');
   }
 
-  // console.log(list);
-  useEffect(() => {
-    console.log('데이터불러오기');
-    // 최초 마운트에 데이터를 불러오기
-    fetchDatas()
-  }, [])
-
-  console.log('전체 컴포넌트 랜더링');
-  const changeHandler = (evt) => {
-    // console.log(evt.target.value);
-    setCategory(evt.target.value)
-  }
-  // useEffect(() => {
-
-  // })
-  // 컴포넌트 랜더링의 초기화, 종료시에 동작할당을 원함?
-  // 컴포넌트의 특정 의존성값에 따라 특정 동작할당을 원함?
   return (
-    <>
-        <span>select view</span>
-        <select onChange={changeHandler}>
-          <option>날짜</option>
-          <option>이름</option>
-          <option>카테고리</option>
-        </select>
-      <h1>{category} view</h1>
-      {loading ? list.map(el => {
-        return (
-            <div key={el.id}>{el.name}{el.date}{el.category}</div>
-        )
-      }): "로딩중이에요"}
-    </>
+  <div>
+    <button onClick={handler}>모달창 열기</button>
+    {open ? createPortal(
+      <Modal mainText = {`Discard`} subText = {'Really want to discard?'} close = {handler}/>, document.body
+    ): null}
+  </div>
   )
 }
 
